e7d6f420eddb92be0406e976388a27a6a39ea7b7
Greg Anderson <greg.1.anderson@greenknowe.org>
2015-12-03 14:51:19 -0800

    Factor out archive extraction into its own Robo task.

--- src/RoboFile.php
+++ src/RoboFile.php
@@ -85,14 +85,19 @@ class RoboFile extends \Robo\Tasks {
     $tmpDir = $this->getTmpDir();
     $archiveName = basename($source);
     $archivePath = "$tmpDir/$archiveName";
-    // $archiveExtension = preg_match("#\.[a-z.]*$#", $archiveName, $matches);
+    $fetchDirName = $this->getFetchDirName();
 
     // Gets the source via wget.
     $fetch = $this->taskExec('wget')
       ->args($source)
       ->args("--output-file=$archivePath");
 
-    return $this->fetchAndPlaceScaffold($webroot, $tmpDir, $excludes, $settingsFiles, [$fetch]);
+    // Once this is merged into Robo, we will be able to simply do:
+    // $extract = $this->tastExtract($archivePath)->to("$tmpDir/$fetchDirName");
+    $extract = new Extract($archivePath);
+    $extract->to("$tmpDir/$fetchDirName");
+
+    return $this->fetchAndPlaceScaffold($webroot, $tmpDir, $excludes, $settingsFiles, [$fetch, $extract]);
   }
 
   protected function fetchAndPlaceScaffold($webroot, $tmpDir, $excludes, $settingsFiles, $ops) {
@@ -143,130 +148,5 @@ class RoboFile extends \Robo\Tasks {
     $this->taskFilesystemStack()
       ->chmod($confDir, $confDirOriginalPerms)
       ->run();
-  }
-
-  protected function extractArchive($archivePath, $destination) {
-    if (!($mimetype = $this->archiveType($archivePath))) {
-      return;
-    }
-
-    // We will first extract to $extractLocation and then move to $destination
-    $extractLocation = $this->getTmpDir();
-    $this->taskFilesystemStack()
-      ->mkdir($extractLocation)
-      ->mkdir(dirname($destination))
-      ->run();
-
-    // Perform the extraction of a zip file.
-    if (($mimetype == 'application/zip') || ($mimetype == 'application/x-zip')) {
-      $this->taskExec("unzip")
-        ->args($archivePath)
-        ->args("-d")
-        ->args("--destination=$extractLocation")
-        ->run();
-    }
-    // Otherwise we have a possibly-compressed Tar file.
-    // If we are not on Windows, then try to do "tar" in a single operation.
-    else {
-      $tar_compression_flag = '';
-      if ($mimetype == 'application/x-gzip') {
-        $tar_compression_flag = 'z';
-      }
-      elseif ($mimetype == 'application/x-bzip2') {
-        $tar_compression_flag = 'j';
-      }
-      $this->taskExec("tar")
-        ->args('-C')
-        ->args($extractLocation)
-        ->args("-x${$tar_compression_flag}f")
-        ->args($archivePath)
-        ->run();
-    }
-
-    // Now, we want to move the extracted files to $destination. There
-    // are two possibilities that we must consider:
-    //
-    // (1) Archived files were encapsulated in a folder with an arbitrary name
-    // (2) There was no encapsulating folder, and all the files in the archive
-    //     were extracted into $extractLocation
-    //
-    // In the case of (1), we want to move and rename the encapsulating folder
-    // to $destination.
-    //
-    // In the case of (2), we will just move and rename $extractLocation.
-    $filesInExtractLocation = glob("$extractLocation/*");
-    $hasEncapsulatingFolder = ((count($filesInExtractLocation) == 1) && is_dir($filesInExtractLocation[0]));
-    if ($hasEncapsulatingFolder) {
-      $this->taskFilesystemStack()
-        ->rename($filesInExtractLocation[0], $destination);
-      $this->taskDeleteDir($extractLocation)->run();
-    }
-    else {
-      $this->taskFilesystemStack()
-        ->rename($extractLocation, $destination);
-    }
-    return $return;
-  }
-
-  protected function archiveType($archivePath) {
-    $content_type = FALSE;
-    if (class_exists('finfo')) {
-      $finfo = new finfo(FILEINFO_MIME_TYPE);
-      $content_type = $finfo->file($filename);
-      // If finfo cannot determine the content type, then we will try other methods
-      if ($content_type == 'application/octet-stream') {
-        $content_type = FALSE;
-      }
-    }
-    // Examing the file's magic header bytes.
-    if (!$content_type) {
-      if ($file = fopen($filename, 'rb')) {
-        $first = fread($file, 2);
-        fclose($file);
-
-        if ($first !== FALSE) {
-          // Interpret the two bytes as a little endian 16-bit unsigned int.
-          $data = unpack('v', $first);
-          switch ($data[1]) {
-            case 0x8b1f:
-              // First two bytes of gzip files are 0x1f, 0x8b (little-endian).
-              // See http://www.gzip.org/zlib/rfc-gzip.html#header-trailer
-              $content_type = 'application/x-gzip';
-              break;
-
-            case 0x4b50:
-              // First two bytes of zip files are 0x50, 0x4b ('PK') (little-endian).
-              // See http://en.wikipedia.org/wiki/Zip_(file_format)#File_headers
-              $content_type = 'application/zip';
-              break;
-
-            case 0x5a42:
-              // First two bytes of bzip2 files are 0x5a, 0x42 ('BZ') (big-endian).
-              // See http://en.wikipedia.org/wiki/Bzip2#File_format
-              $content_type = 'application/x-bzip2';
-              break;
-          }
-        }
-      }
-    }
-    // 3. Lastly if above methods didn't work, try to guess the mime type from
-    // the file extension. This is useful if the file has no identificable magic
-    // header bytes (for example tarballs).
-    if (!$content_type) {
-      // Remove querystring from the filename, if present.
-      $filename = basename(current(explode('?', $filename, 2)));
-      $extension_mimetype = array(
-        '.tar.gz'  => 'application/x-gzip',
-        '.tgz'     => 'application/x-gzip',
-        '.tar'     => 'application/x-tar',
-      );
-      foreach ($extension_mimetype as $extension => $ct) {
-        if (substr($filename, -strlen($extension)) === $extension) {
-          $content_type = $ct;
-          break;
-        }
-      }
-    }
-    return $content_type;
   }
 }
