d36fbf9451b8931ebee057e6282d600fad072788
Carlos Jimenez <Saphyel@gmail.com>
2017-06-26 12:30:11 +0100

    Return true by default

--- src/Plugin.php
+++ src/Plugin.php
@@ -77,5 +77,6 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
     $handler->downloadScaffold();
     // Generate the autoload.php file after generating the scaffold files.
     $handler->generateAutoload();
+    return true;
   }
 }
